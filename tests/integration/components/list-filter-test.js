6ab7ce07bd402a4fe9b6194c8c5c310efb13cedb
Guillaume Maiano <guillaume@maiano.fr>
2017-06-26 02:11:45 +0200

    Time for testing

---
+++ tests/integration/components/list-filter-test.js
@@ -0,0 +1,9 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('list-filter', 'Integration | Component | filter listing', {
+  integration: true
+});
+
+test('should initially load all listings', function (assert) {
+});
