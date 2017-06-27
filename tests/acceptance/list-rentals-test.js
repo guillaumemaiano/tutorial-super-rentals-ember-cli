a8a551e242752c07e08bfd1f979e65a675f25b79
Guillaume Maiano <guillaume@maiano.fr>
2017-06-27 15:09:58 +0200

    Adds acceptance test for details of properties

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -65,4 +65,14 @@ test('Should filter the list of rentals per city', function(assert) {
                 }
                );
         });
-test('Should show details for a selected rental', function(assert) {});
+test('Should show details for a selected rental', function(assert) {
+    visit('/rentals');
+    click('a:contains("Grand Old Mansion")');
+    andThen(
+        function() {
+            assert.equal(currentUrl(), '/rentals/grand-old-mansion', 'should navigate to show routes');
+            assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+            assert.equal(find('.description').length, 1, "should list a description for the (mansion) property");
+        }
+    );
+});
